afc2191f55d28b5779856c7123d33d1b20b76516
Riley Shaw <rileyjshaw@gmail.com>
2014-09-07 18:52:19 -0700

    Add touch events

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ function spin () {
   if (spinning) requestAnimationFrame(spin);
 }
 
+function start () {
+  spinning = true;
+  spin();
+}
+
+function stop () { spinning = false; }
+
 var spinning = false;
 var p = document.createElement('p');
 spin();
 
-p.addEventListener('mouseenter', function () {
-  spinning = true;
-  spin();
-}, false);
-
-p.addEventListener('mouseleave', function () { spinning = false; }, false);
+p.addEventListener('mouseenter', start, false);
+p.addEventListener('touchstart', start, false);
+p.addEventListener('mouseleave', stop, false);
+p.addEventListener('touchend', stop, false);
 
 document.body.appendChild(p);
